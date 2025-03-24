831c1202c33c807a82f32f3c108dcd970a125079
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-24 09:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+const removeButton = document.querySelector('input[type="button"][value="Select and Remove"]');
+
+removeButton.addEventListener("click", () => {
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+  if (selectedOption) {
+    colorSelect.removeChild(selectedOption);
+  }
+});
